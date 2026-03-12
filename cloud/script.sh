5b751314fed071dedfe68f46a3ff6286dcc3ae2a
RaphaelVERGINELLI <raphael.verginelli@orange.fr>
2026-03-12 14:29:02 +0100

    correction shell script and ignore pytest vulnerability

--- cloud/script.sh
+++ cloud/script.sh
@@ -1,18 +1,9 @@
- #!/bin/bash
-# ShellCheck Error: Variable is unquoted, may break on spaces
-echo $USER_INPUT
+!/bin/bash
+# Properly quoted variables to satisfy ShellCheck
+echo "${USER_INPUT}"
 
-# ShellCheck Error: Don't use ls to iterate over files, use globs
-for f in $(ls *.py); do
-    cat $f
+# Proper globbing for file iteration
+for f in *.py; do
+    [ -e "$f" ] || continue
+    cat "$f"
 done
-
-#!/bin/bash
-# # Properly quoted variables to satisfy ShellCheck
-# echo "${USER_INPUT}"
-
-# # Proper globbing for file iteration
-# for f in *.py; do
-#     [ -e "$f" ] || continue
-#     cat "$f"
-# done
