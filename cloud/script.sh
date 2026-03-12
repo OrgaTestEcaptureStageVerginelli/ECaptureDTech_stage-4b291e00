b5a2558052b3aba26e852d67b8e5d0b8d715f990
RaphaelVERGINELLI <raphael.verginelli@orange.fr>
2026-03-12 12:37:13 +0100

    Commit shell and dockerfile error script

---
+++ cloud/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ShellCheck Error: Variable is unquoted, may break on spaces
+echo $USER_INPUT
+
+# ShellCheck Error: Don't use ls to iterate over files, use globs
+for f in $(ls *.py); do
+    cat $f
+done
+
+
+# #!/bin/bash
+# # Properly quoted variables to satisfy ShellCheck
+# echo "${USER_INPUT}"
+
+# # Proper globbing for file iteration
+# for f in *.py; do
+#     [ -e "$f" ] || continue
+#     cat "$f"
+# done
