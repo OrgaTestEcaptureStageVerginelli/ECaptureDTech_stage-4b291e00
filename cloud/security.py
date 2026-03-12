67377ba0bf5f48f442d640c1d1aac1cc354ad007
RaphaelVERGINELLI <raphael.verginelli@orange.fr>
2026-03-12 12:30:02 +0100

    Commit python error script

--- cloud/security.py
+++ cloud/security.py
@@ -15,23 +15,3 @@ class TaskManager:
 x = 5
 
 
-# """Module providing secure task management utilities."""
-
-# import subprocess
-# from typing import List
-
-# class TaskManager:
-#     """Handles task execution securely."""
-
-#     def execute_task(self, cmd: str) -> None:
-#         """Executes a command using a secure list-based approach.
-
-#         Args:
-#             cmd: The command string to echo.
-#         """
-#         # Secure execution without shell=True to satisfy Bandit
-#         subprocess.run(["echo", cmd], check=True)
-
-# def get_status() -> List[str]:
-#     """Returns the current system status."""
-#     return ["online", "secure"]
